6cd519ba8f259ffc1dec54eeba87b418e196ca88
samk257 <kubuntusampras.sk@gmail.com>
2022-11-07 19:12:04 +0200

    adding index,store and resource in web.php

---
+++ database/migrations/2022_11_07_163859_create_bugdets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bugdets', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('type');
+            $table->mediumText('description');
+            $table->integer('number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bugdets');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/addTask',[TaskController::class,'index'])->name('addTask');
+Route::resource('tasks', TaskController::class);
 
